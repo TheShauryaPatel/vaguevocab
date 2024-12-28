69c86f19840717ecc7d89fa2dad0f84feee3daf4
TheShauryaPatel <159413409+TheShauryaPatel@users.noreply.github.com>
2024-12-27 22:39:40 -0600

    Update settings.py
    unexpected indent, im stupid

--- vaguevocab/settings.py
+++ vaguevocab/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
- STATICFILES_DIRS = [
+STATICFILES_DIRS = [
     BASE_DIR / "static",  # This points to the static folder at the root of the project
 ]
 
